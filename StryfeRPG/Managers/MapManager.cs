43f5966c1d836ae73ab69c1cde922212a663c501
Guilherme Paz Silva <pazsilvag@gmail.com>
2016-02-04 16:25:05 -0200

    First commit.

---
+++ StryfeRPG/Managers/MapManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StryfeRPG.Managers
+{
+    public class MapManager
+    {
+        private static MapManager instance;
+
+        protected MapManager() { }
+
+        public static MapManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MapManager();
+                }
+                return instance;
+            }
+        }
+
+        public void LoadMap(string mapName)
+        {
+            
+        }
+    }
+}
---
+++ StryfeRPG/Models/Maps/Map.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StryfeRPG.Models.Maps
+{
+    class Map
+    {
+    }
+}
