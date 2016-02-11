d1a870eb02e75b99a94b72a671c6f524da809fea
Guilherme Paz Silva <pazsilvag@gmail.com>
2016-02-11 16:55:11 -0200

    Added ObjectInfo model;

---
+++ StryfeRPG/Models/Maps/ObjectInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StryfeRPG.Models.Maps
+{
+    public class ObjectInfo
+    {
+        public string ObjectIdentifier { get; set; } // The identifier of the object (id_name_map)
+        public int NumberOfInteractions { get; set; } // Number of times the player interacted with it
+        public bool IsActive { get; set; } // If the object is active in the map
+    }
+}
